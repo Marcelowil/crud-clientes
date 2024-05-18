46a93047d7fcc78b59ca6351ecf4e11ec9a93264
Marcelo William <marcelo.willian@live.com>
2024-05-18 17:52:29 -0300

    Client Repository

---
+++ src/main/java/com/client/crud/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.client.crud.repositories;
+
+import com.client.crud.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Long, Client> {
+}
